d8027660693996e9a6fb1b911b910a60d6736ae7
benbousquet <bbousque@asu.edu>
2020-11-09 15:48:40 -0700

    feat(て form practice): created structure for component

--- src/pages/teForm.js
+++ src/pages/teForm.js
@@ -1,7 +1,25 @@
+import { Box, Button, Flex, Heading, Input, Text } from "@chakra-ui/core";
+
 export default function TeForm() {
   return (
-    <div>
-      <p>Teform</p>
-    </div>
+    <Flex flexDirection="row" justifyContent="center" paddingTop="1rem">
+      <Flex
+        borderWidth="1px"
+        borderRadius="8px"
+        width="45rem"
+        padding="1rem"
+        flexDirection="column"
+        alignItems="center"
+      >
+        <Heading textAlign="center">Te Form Practice</Heading>
+        <Box borderWidth="1px" shadow="md" padding="1rem 2rem" margin="1rem">
+          <Text>This is a test</Text>
+        </Box>
+        <Flex flexDirection="row" width="30rem" justifyContent="space-evenly">
+          <Input placeholder="answer" width="15rem" />
+          <Button>Submit</Button>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 }
