b73d38ab5f2d4a9dca4dd407e665cb88c01f6944
benbousquet <bbousque@asu.edu>
2020-11-09 20:53:14 -0700

    feat(statistics): added ability to toggle statistics

--- src/pages/teForm.js
+++ src/pages/teForm.js
@@ -1,16 +1,22 @@
 import { useState } from "react";
-import { Flex } from "@chakra-ui/core";
+import { Button, Collapse, Flex } from "@chakra-ui/core";
 import Statistics from "../components/statistics";
 import TeFormApp from "../components/teFormApp";
 
 export default function TeForm() {
+  let [showStats, setShowStats] = useState(true);
   let [score, setScore] = useState({ correct: 0, incorrect: 0 });
   // {question, answer, result}
   let [history, setHistory] = useState([]);
   return (
     <Flex flexDirection="column" alignItems="center" paddingTop="1rem">
-      <Statistics score={[score, setScore]} history={[history, setHistory]} />
+      <Button onClick={() => setShowStats(!showStats)} marginBottom="1rem">
+        Toggle Stats
+      </Button>
       <TeFormApp score={[score, setScore]} history={[history, setHistory]} />
+      <Collapse isOpen={showStats} marginTop="1rem">
+        <Statistics score={[score, setScore]} history={[history, setHistory]} />
+      </Collapse>
     </Flex>
   );
 }
